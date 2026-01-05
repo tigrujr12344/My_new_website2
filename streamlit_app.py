6f1e447a3bf7399d4c1363973cf7a4ef75b5bf55
tigrujr12344 <tigrujr24@gmail.com>
2026-01-05 02:44:39 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("My new website!", divider="rainbow")
+st.markdown("This is a prototype!")
